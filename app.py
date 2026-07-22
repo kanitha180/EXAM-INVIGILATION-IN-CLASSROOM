5da00c391ed23e4405f350e63361bc2c32988cc3
Kanitha <kanithak180@gmail.com>
2026-07-22 10:30:34 +0530

    Created AI homepage and monitor page

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/monitor")
+def monitor():
+    return render_template("monitor.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
