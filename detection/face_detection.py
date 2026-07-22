a202afc68388f737a93c36ed4611e9ab72ffabb3
Kanitha <kanithak180@gmail.com>
2026-07-22 11:43:09 +0530

    Added multiple face detection with student alerts

--- detection/face_detection.py
+++ detection/face_detection.py
@@ -2,19 +2,27 @@ import cv2
 import mediapipe as mp
 
 mp_face = mp.solutions.face_detection
+
 face_detection = mp_face.FaceDetection(
     model_selection=0,
     min_detection_confidence=0.5
 )
 
 def detect_faces(frame):
+
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = face_detection.process(rgb)
 
+    face_count = 0
+
     if results.detections:
+
+        face_count = len(results.detections)
+
         h, w, _ = frame.shape
 
         for detection in results.detections:
+
             bbox = detection.location_data.relative_bounding_box
 
             x = int(bbox.xmin * w)
@@ -22,10 +30,51 @@ def detect_faces(frame):
             bw = int(bbox.width * w)
             bh = int(bbox.height * h)
 
-            cv2.rectangle(frame,
-                          (x, y),
-                          (x + bw, y + bh),
-                          (0, 255, 0),
-                          2)
+            cv2.rectangle(frame, (x, y), (x+bw, y+bh), (0,255,0), 2)
+
+    cv2.putText(
+        frame,
+        f"Faces : {face_count}",
+        (20,80),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255,255,0),
+        2
+    )
+
+    # No Face Alert
+    if face_count == 0:
+        cv2.putText(
+            frame,
+            "ALERT : No Face Detected",
+            (20,120),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0,0,255),
+            3
+        )
+
+    # Multiple Face Alert
+    elif face_count > 1:
+        cv2.putText(
+            frame,
+            "ALERT : Multiple Faces Detected",
+            (20,120),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0,0,255),
+            3
+        )
+
+    else:
+        cv2.putText(
+            frame,
+            "STATUS : Normal",
+            (20,120),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0,255,0),
+            2
+        )
 
     return frame
